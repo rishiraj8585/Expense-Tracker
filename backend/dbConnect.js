e704d14bea44a81409a9ca8c79a42ad87984401b
rishiraj8585 <120587646+rishiraj8585@users.noreply.github.com>
2024-08-31 14:56:51 +0530

    Update dbConnect.js

--- backend/dbConnect.js
+++ backend/dbConnect.js
@@ -1,5 +1,5 @@
 const mongoose=require('mongoose');
-const url='mongodb+srv://rajveeersingh1505:VZJdDdV9B3JbTJNx@cluster0.hf6yx.mongodb.net';
+const url='mongodb+srv://rishirajmotion123:jtD9rA57F0Y32Npo@cluster0.7ayv5.mongodb.net/';
 mongoose.connect(url,{useUnifiedTopology: true});
 
 const connection=mongoose.connection;
